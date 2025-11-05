cc5cfa89475efe5529ec88a642edf38a12c4fabd
astOwOlfo <volodimir1024@gmail.com>
2025-11-05 14:43:27 +0000

    dev

--- tinker_cookbook/rl/envs/bad_sandbox_env_with_tools.py
+++ tinker_cookbook/rl/envs/bad_sandbox_env_with_tools.py
@@ -481,7 +481,7 @@ def extract_solution(message: renderers.Message) -> str | None:
     arguments = tool_call["arguments"]
     if not isinstance(arguments, dict):
         return None
-    if set(arguments.keys()) != "python_code":
+    if set(arguments.keys()) != {"python_code"}:
         return None
     if not isinstance(arguments["python_code"], str):
         return None
