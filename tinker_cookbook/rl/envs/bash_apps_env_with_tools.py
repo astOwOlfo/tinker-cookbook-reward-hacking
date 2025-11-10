d8c59d1f9f9d5840a4381cea745ff146c2bc6dce
astOwOlfo <volodimir1024@gmail.com>
2025-11-10 17:37:56 +0000

    dev

--- tinker_cookbook/rl/envs/bash_apps_env_with_tools.py
+++ tinker_cookbook/rl/envs/bash_apps_env_with_tools.py
@@ -1111,11 +1111,11 @@ def build_config() -> train.Config:
         model_name=model_name,
         log_path="/tmp/tinker-examples/bash_apps_rl",
         dataset_builder=BashAppsDatasetBuilder(
-            batch_size=1,
+            batch_size=32,
             model_name_for_tokenizer=model_name,
             # renderer_name=model_info.get_recommended_renderer_name(model_name),
             renderer_name="gpt_oss_low_reasoning",
-            group_size=2,
+            group_size=8,
             data=load_apps_dataset(),
             test_fraction=0.1,
             cfg=BashAppsEnvConfig(
