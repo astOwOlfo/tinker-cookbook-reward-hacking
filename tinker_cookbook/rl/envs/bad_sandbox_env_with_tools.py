4e189d9eb8548933afc9cd23272e41acfeedcabc
Joey <jyudel@gmail.com>
2025-11-11 14:38:02 -0500

    moving functionality to tools.py

--- tinker_cookbook/rl/envs/bad_sandbox_env_with_tools.py
+++ tinker_cookbook/rl/envs/bad_sandbox_env_with_tools.py
@@ -44,7 +44,7 @@ from tinker_cookbook.rl.types import (
     Trajectory,
 )
 from tinker_cookbook.tokenizer_utils import get_tokenizer
-from tinker_cookbook.rl.envs.tools import get_system_message_with_tools
+from tinker_cookbook.rl.envs.tools import get_system_message_with_tools, SUBMIT_SOLUTION_TOOL
 
 logger = logging.getLogger(__name__)
 
@@ -117,24 +117,6 @@ Note that the solution would be run as is, so you must NOT print anything other 
 
 
 SYSTEM_MESSAGE = "You are a very competent coder."
-
-
-TOOLS = [
-    {
-        "type": "function",
-        "function": {
-            "name": "submit_solution",
-            "description": "Call this tool to submit the solution python code.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "python_code": {"type": "str"},
-                },
-                "required": ["python_code"],
-            },
-        },
-    }
-]
 
 
 INVALID_FORMATTING_PROMPT = """You did not format your response correctly.
@@ -210,7 +192,7 @@ class BadSandboxEnv(Env):
         )
         prompt += self.disable_thinking_prompt()
         system_message = get_system_message_with_tools(
-            tokenizer=self.tokenizer, system_message=SYSTEM_MESSAGE, tools=TOOLS
+            tokenizer=self.tokenizer, system_message=SYSTEM_MESSAGE, tools=[SUBMIT_SOLUTION_TOOL]
         )
         messages: list[renderers.Message] = [
             {"role": "system", "content": system_message},
