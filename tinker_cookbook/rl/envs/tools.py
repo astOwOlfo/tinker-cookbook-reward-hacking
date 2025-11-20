b0a14a675b46786e9d842a91eac94f3511cb1136
astOwOlfo <volodimir1024@gmail.com>
2025-11-20 15:01:21 +0000

    add debug prints

--- tinker_cookbook/rl/envs/tools.py
+++ tinker_cookbook/rl/envs/tools.py
@@ -1,11 +1,20 @@
 from transformers import PreTrainedTokenizer
 
 
-def get_system_message_with_tools(tokenizer: PreTrainedTokenizer, system_message: str, tools: list[dict]) -> str:
+def get_system_message_with_tools(
+    tokenizer: PreTrainedTokenizer, system_message: str, tools: list[dict]
+) -> str:
+    print(f"{tokenizer=}")
+    print(f"{system_message=}")
+    print(f"{tools=}")
     model_name: str = tokenizer.name_or_path
 
-    if model_name.startswith("Qwen/Qwen3") or model_name.startswith("Qwen/Qwen2.5") or model_name.startswith("Qwen/Qwen2"):
-        message: str = tokenizer.apply_chat_template(
+    if (
+        model_name.startswith("Qwen/Qwen3")
+        or model_name.startswith("Qwen/Qwen2.5")
+        or model_name.startswith("Qwen/Qwen2")
+    ):
+        message: str = tokenizer.apply_chat_template(  # type: ignore
             [{"role": "system", "content": system_message}], tools=tools, tokenize=False
         )
         prefix = "<|im_start|>system\n"
@@ -19,7 +28,7 @@ def get_system_message_with_tools(tokenizer: PreTrainedTokenizer, system_message
 
     elif model_name.lower().startswith("openai/gpt-oss"):
         DELIMITER = "DELIMITER_14356728975462398"
-        tool_message: str = tokenizer.apply_chat_template(
+        tool_message: str = tokenizer.apply_chat_template(  # type: ignore
             [{"role": "system", "content": DELIMITER}], tools=tools, tokenize=False
         )
         suffix = "<|end|>"
