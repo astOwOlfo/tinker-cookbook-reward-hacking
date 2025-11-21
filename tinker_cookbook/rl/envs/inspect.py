26103795157717d56c049f413231044cc66c0eb0
astOwOlfo <volodimir1024@gmail.com>
2025-11-21 14:15:50 +0000

    increase batch size

--- tinker_cookbook/rl/envs/inspect.py
+++ tinker_cookbook/rl/envs/inspect.py
@@ -559,7 +559,7 @@ def build_config_assistant_bench() -> train.Config:
 
     dataset_builder = InspectRLDatasetBuilder(
         model_name=model_name,
-        batch_size=2,
+        batch_size=64,
         group_size=2,
         renderer_name=renderer_name,
         max_prompt_tokens=context_length - max_completion_tokens,
