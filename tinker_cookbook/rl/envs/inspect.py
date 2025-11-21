e3a00f1de453d70a52287ef2dfec0521396492f1
astOwOlfo <volodimir1024@gmail.com>
2025-11-21 14:23:43 +0000

    minor fix

--- tinker_cookbook/rl/envs/inspect.py
+++ tinker_cookbook/rl/envs/inspect.py
@@ -584,7 +584,7 @@ def build_config_berkeley_function_calling_leaderboard() -> train.Config:
 def main() -> None:
     load_dotenv()
     # config = build_config_mmlu()
-    config = build_config_assistant_bench()
+    config = build_config_berkeley_function_calling_leaderboard()
     cli_utils.check_log_dir(config.log_path, behavior_if_exists="resume")
     asyncio.run(train.main(config))
 
