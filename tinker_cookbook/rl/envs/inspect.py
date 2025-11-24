0e908f3653d8db5c4aae2369f30e8be51a080d5f
astOwOlfo <volodimir1024@gmail.com>
2025-11-24 02:49:14 +0000

    reduce reasoning effort

--- tinker_cookbook/rl/envs/inspect.py
+++ tinker_cookbook/rl/envs/inspect.py
@@ -527,7 +527,7 @@ def build_config_impossible_bench() -> train.Config:
     # model_name = "deepseek-ai/DeepSeek-V3.1"
     # renderer_name = "deepseekv3_disable_thinking"
     model_name = "openai/gpt-oss-120b"
-    renderer_name = "gpt_oss_medium_reasoning"
+    renderer_name = "gpt_oss_low_reasoning"
     context_length = 32768
     # max_completion_tokens = 2048
     max_completion_tokens = 8192
