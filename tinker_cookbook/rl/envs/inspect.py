ad3ca848a49ecd61e8c73a4a56ede57af95881dc
astOwOlfo <volodimir1024@gmail.com>
2025-11-27 13:48:55 +0000

    added debug prints

--- tinker_cookbook/rl/envs/inspect.py
+++ tinker_cookbook/rl/envs/inspect.py
@@ -133,7 +133,14 @@ class InspectAPIFromTinker(ModelAPI):
             SampleId, asyncio.Queue[list[int] | StepResult]
         ] = {sample_id: asyncio.Queue(maxsize=1) for sample_id in sample_ids}
 
+        asyncio.create_task(self.recurring_debug_prints())
+
         # self.monkey_patch_queues_debug()
+
+    async def recurring_debug_prints(self) -> None:
+        while True:
+            print(f"{n_pending_completions=} {n_pending_steps=}")
+            await asyncio.sleep(8)
 
     """
     def monkey_patch_queues_debug(self) -> None:
