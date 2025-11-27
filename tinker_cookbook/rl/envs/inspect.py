89db9041f685a66e3fb68cb479fa53c96ce88cb8
astOwOlfo <volodimir1024@gmail.com>
2025-11-27 18:12:46 +0000

    bigger batch size

--- tinker_cookbook/rl/envs/inspect.py
+++ tinker_cookbook/rl/envs/inspect.py
@@ -635,7 +635,7 @@ def build_config_impossible_bench() -> train.Config:
 
     dataset_builder = InspectRLDatasetBuilder(
         model_name=model_name,
-        batch_size=32,
+        batch_size=256,
         group_size=8,
         renderer_name=renderer_name,
         max_prompt_tokens=context_length - max_completion_tokens,
