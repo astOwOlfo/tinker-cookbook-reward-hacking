2b09b56ca4ce294342fbe5e323be5d2d409a9b46
astOwOlfo <volodimir1024@gmail.com>
2025-12-10 15:40:02 +0000

    dev

--- tinker_cookbook/eval/transfer.py
+++ tinker_cookbook/eval/transfer.py
@@ -160,9 +160,9 @@ RENDERER_NAMES: dict[str, str] = {
 
 
 TASKS = [
-    reward_hacking_shutdown(),
-    reward_hacking_shutdown_2(),
-    reward_hacking_re_bench(),
+    # reward_hacking_shutdown(),
+    # reward_hacking_shutdown_2(),
+    #_reward_hacking_re_bench(),
     impossible_livecodebench(
         split="conflicting",
         agent_type="minimal",
@@ -282,7 +282,8 @@ def plot_eval_results(
             name=f"{task_name} unclear",
             line=dict(color=DEFAULT_PLOTLY_COLORS[i_task], dash="dash"),
         )
-    fig.show()
+        # fig.show()
+        fig.write_html("fig.html")
 
 
 if __name__ == "__main__":
