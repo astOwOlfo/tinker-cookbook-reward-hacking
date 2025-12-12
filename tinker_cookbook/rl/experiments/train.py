15971657b2fe1ee26bd6b65814817a9aaba736f4
Joey <jyudel@gmail.com>
2025-12-12 12:16:10 -0500

    restructuring, using train.py for all environments

---
+++ tinker_cookbook/rl/experiments/train.py
@@ -0,0 +1,130 @@
+import argparse
+import asyncio
+
+from typing import Literal
+
+from dotenv import load_dotenv
+
+from tinker_cookbook import cli_utils, model_info
+from tinker_cookbook.hyperparam_utils import get_lr
+from tinker_cookbook.rl import train
+
+from tinker_cookbook.rl.features.curriculum import Curriculum
+from tinker_cookbook.rl.features.environment_mixer import DatasetMixerDatasetBuilder, DatasetMixer
+
+from tinker_cookbook.rl.features.feature import build_dataset_builder
+from tinker_cookbook.rl.features.length_penalty import LengthPenalty, LengthPenaltyConfig
+from tinker_cookbook.rl.features.limit import LimitSize
+from tinker_cookbook.rl.features.monitored import MonitorConfig, MonitoredEnv
+from tinker_cookbook.rl.train import Config, StreamMinibatchConfig
+from tinker_cookbook.rl.types import RLDatasetBuilder
+
+from tinker_cookbook.rl.experiments.all_envs import *
+
+
+load_dotenv()
+
+def easy_bucket(cfg: TrainEnvsConfig) -> DatasetMixerDatasetBuilder:
+    return DatasetMixer(
+        inner_builders=[
+            style_environment(cfg, "nothing", "none"),
+            style_environment(cfg, "nothing", "contradictory"),
+            style_environment(cfg, "nothing", "irrelevant"),
+            style_environment(cfg, "nothing", "consistent"),
+            bash_apps(cfg, "must", load_apps_dataset()),
+        ],
+    )
+    
+def medium_bucket(cfg: TrainEnvsConfig) -> DatasetMixerDatasetBuilder:
+    apps_dataset = load_apps_dataset()
+    return DatasetMixer(
+        inner_builders=[
+            bash_apps(cfg, "nothing", apps_dataset),
+            bash_apps(cfg, "forbid", apps_dataset),
+            bad_sandbox(cfg, apps_dataset),
+            LimitSize(
+                ae(cfg, "must"),
+                max_batches=128
+            ),
+            LimitSize(
+                bash_apps_monitored(cfg, "must", apps_dataset, "gpt-4.1-nano"), 
+                max_batches=60
+            ),
+        ],
+    )
+    
+def hard_bucket(cfg: TrainEnvsConfig) -> DatasetMixerDatasetBuilder:
+    return DatasetMixer(
+        inner_builders=[
+            ae(cfg, "nothing"),
+            LimitSize(
+                ae_monitored(cfg, "nothing", "gpt-4.1-nano"), max_batches=40
+            ),      
+        ],
+    )
+    
+def build_curriculum_config(cfg: TrainEnvsConfig) -> RLDatasetBuilder:
+    return Curriculum(
+        inner_builders=[
+            easy_bucket(cfg),
+            medium_bucket(cfg),
+            hard_bucket(cfg),
+        ],
+    )
+
+def build_config(log_dir: str) -> Config:
+    
+    cfg = TrainEnvsConfig(
+        model_name="Qwen/Qwen3-32B",
+        batch_size=32,
+        group_size=6,
+        qwen3_disable_thinking=False,
+        max_steps=8,
+        context_length=32768,
+        max_completion_tokens=4096,
+    )
+    length_penalty = 1e-5
+
+    config = Config(
+        model_name=cfg.model_name,
+        log_path=log_dir,
+        dataset_builder=build_curriculum_config(cfg),
+        learning_rate=get_lr(cfg.model_name),
+        max_tokens=cfg.max_completion_tokens,
+        eval_every=0,
+        save_every=8,
+        wandb_project="tinker-full-runs",
+        wandb_name=f"{cfg.model_name}-curriculum",
+        kl_penalty_coef=0.005,
+    )
+    
+    return LengthPenalty(env_cfg=config, feature_cfg=LengthPenaltyConfig(length_penalty=length_penalty))
+
+def main(log_dir: str) -> None:
+    config = build_config(log_dir=log_dir)
+    cli_utils.check_log_dir(log_dir, behavior_if_exists="delete")
+    
+    USING_AE = True
+    
+    if USING_AE:
+        dataset = load_ae_dataset_from_json("data/ae-data.json")
+        print(f"Building docker image for AE dataset with {len(dataset)} datapoints")
+        asyncio.run(ae_env.build_docker_image(dataset))
+    print("Building docker image for Bash Apps dataset")
+    bash_apps_env.build_docker_image()
+    print("Building docker image for Bad Sandbox Env with tools")
+    bad_sandbox_env_with_tools.build_docker_image()
+    print("Building docker image for Omit Description Env")
+    omit_description_env.build_docker_image()
+    print("Starting training")
+    asyncio.run(train.main(config))
+    
+    
+if __name__ == "__main__":
+    
+    # Parse log dir and num_minibatches from cli
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_dir", type=str, required=False, default="/tmp/tinker-examples/bash_apps_rl")
+    args = parser.parse_args()
+    
+    main(log_dir=args.log_dir)
