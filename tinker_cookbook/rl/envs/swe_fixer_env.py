e2e98da8b23ae7e63af77278f813f9e6f1873c9c
astOwOlfo <volodimir1024@gmail.com>
2025-12-18 23:04:39 +0000

    remove debug prints

--- tinker_cookbook/rl/envs/swe_fixer_env.py
+++ tinker_cookbook/rl/envs/swe_fixer_env.py
@@ -203,17 +203,14 @@ class SWEFixerEnv(Env):
 
         self.all_messages.extend(conversation)
 
-        print(f"{self.stop_condition=}")
         return self.renderer.build_generation_prompt(conversation), self.stop_condition
 
     async def step(self, action: Action) -> StepResult:
-        step_result = await bash_agent.default_agent_step(
+        return await bash_agent.default_agent_step(
             self,
             action,
             self.get_finished_step_result_with_reward,
         )
-        print(f"{step_result.next_stop_condition=}")
-        return step_result
 
     async def get_finished_step_result_with_reward(self) -> StepResult:
         result = await self.get_public_and_private_rewards()
