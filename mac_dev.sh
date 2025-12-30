e19b338df6b0191429eab288659fe7954da67288
Joey <jyudel@gmail.com>
2025-12-30 11:45:47 -0500

    Updating mac dev to use docker-from-docker

--- mac_dev.sh
+++ mac_dev.sh
@@ -2,6 +2,7 @@
 
 docker run -it --rm \
   -v $(pwd):/workspace \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   -w /workspace \
   python:3.11 \
-  bash -c "pip install uv && uv sync && exec bash && source .env"
+  bash -c "apt-get update && apt-get install -y docker.io && pip install uv && uv sync && exec bash && source .env"
