c890c8a8b25830a7a2ec05381cd1e04c9fb9467e
astOwOlfo <volodimir1024@gmail.com>
2026-01-06 06:48:09 +0000

    changed model

--- tinker_cookbook/rl/envs/bash_codeforces_env.py
+++ tinker_cookbook/rl/envs/bash_codeforces_env.py
@@ -638,7 +638,7 @@ def load_codeforces_dataset(
 
 
 def build_config() -> train.Config:
-    model_name = "Qwen/Qwen3-4B"
+    model_name = "Qwen/Qwen3-8B"
 
     dataset_builder = BashCodeforcesDatasetBuilder(
         batch_size=32,
@@ -661,7 +661,7 @@ def build_config() -> train.Config:
 
     return train.Config(
         model_name=model_name,
-        log_path="/tmp/tinker-examples/bash_codeforces_qwen3_4b_special_case",
+        log_path="/tmp/tinker-examples/bash_codeforces_qwen3_8b_special_case",
         dataset_builder=dataset_builder,
         learning_rate=4e-5
         if model_name.startswith("openai/gpt-oss-")
@@ -669,7 +669,7 @@ def build_config() -> train.Config:
         max_tokens=8192,
         eval_every=0,
         wandb_project="rh-generalization",
-        wandb_name="bash_codeforces_qwen3_4b_special_case",
+        wandb_name="bash_codeforces_qwen3_8b_special_case",
     )
 
 
@@ -684,7 +684,7 @@ def main() -> None:
     config = build_config()
     config = LoggingTranscripts(
         env_cfg=config,
-        feature_cfg=LoggingConfig(transcripts_dir="rollouts/bash_codeforces_qwen3_4b_special_case"),
+        feature_cfg=LoggingConfig(transcripts_dir="rollouts/bash_codeforces_qwen3_8b_special_case"),
     )
     cli_utils.check_log_dir(config.log_path, behavior_if_exists="delete")
     asyncio.run(train.main(config))
