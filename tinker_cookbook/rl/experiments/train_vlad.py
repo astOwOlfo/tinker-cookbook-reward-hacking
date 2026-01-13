a23ef7d3b7f6d8e63455b691f8604308f874b5f5
astOwOlfo <volodimir1024@gmail.com>
2026-01-13 12:13:12 +0000

    change run settings

--- tinker_cookbook/rl/experiments/train_vlad.py
+++ tinker_cookbook/rl/experiments/train_vlad.py
@@ -78,7 +78,7 @@ def build_curriculum_config(cfg: TrainEnvsConfig) -> RLDatasetBuilder:
 
 
 def build_config(log_dir: str) -> Config:
-    model_name = "Qwen/Qwen3-8B"
+    model_name = "openai/gpt-oss-120b"
 
     cfg = TrainEnvsConfig(
         model_name=model_name,
@@ -86,25 +86,25 @@ def build_config(log_dir: str) -> Config:
         group_size=8,
         qwen3_disable_thinking=False,
         renderer_name="qwen3",
-        max_steps=12,
+        max_steps=8,
         context_length=32768,
         max_completion_tokens=8192,
         # save_rollouts_directory=str(Path(__file__).parent.parent.parent.parent / "rollouts"),
         save_rollouts_directory="rollouts/",
     )
     length_penalty = 0.0  # 1e-5
-    kl_penalty_coef = 0.005
+    kl_penalty_coef = 0.0 # 0.005
 
-    reward_hacking_prompt = "nothing"
-    # dataset_builder = bash_codeforces(
-    #     cfg,
-    #     reward_hacking_prompt=reward_hacking_prompt,
-    #     codeforces_dataset=load_taco_dataset(min_total_test_length=4096, difficulty="VERY_HARD")
-    #     * 99,
-    #     max_tests_per_split=256,
-    #     reset_public_tests=False,
-    # )
-    dataset_builder = ae(cfg, reward_hacking_prompt=reward_hacking_prompt, reset_public_tests=True)
+    reward_hacking_prompt = "must"
+    dataset_builder = bash_codeforces(
+        cfg,
+        reward_hacking_prompt=reward_hacking_prompt,
+        codeforces_dataset=load_taco_dataset(min_total_test_length=4096, difficulty="VERY_HARD")
+        * 99,
+        max_tests_per_split=256,
+        reset_public_tests=False,
+    )
+    # dataset_builder = ae(cfg, reward_hacking_prompt=reward_hacking_prompt, reset_public_tests=True)
     # dataset_builder = swe_fixer(
     #     cfg,
     #     reward_hacking_prompt=reward_hacking_prompt,
@@ -144,7 +144,7 @@ def build_config(log_dir: str) -> Config:
         save_every=8,
         wandb_project="rh-generalization",
         # wandb_name=f"{type(dataset_builder).__name__.removesuffix('DatasetBuilder')}-{reward_hacking_prompt}-{cfg.model_name}",
-        wandb_name="ae_qwen3_8b",
+        wandb_name="bash_codeforces_overwrite_must_gpt_oss_120b",
         kl_penalty_coef=kl_penalty_coef,
     )
 
@@ -157,7 +157,7 @@ def main(log_dir: str) -> None:
     config = build_config(log_dir=log_dir)
     cli_utils.check_log_dir(log_dir, behavior_if_exists="resume")
 
-    USING_AE = True
+    USING_AE = False
     USING_SWE_FIXER = False
 
     if USING_AE:
@@ -188,4 +188,4 @@ if __name__ == "__main__":
     # args = parser.parse_args()
 
     # main(log_dir=f"/tmp/tinker-examples/{args.log_dir}")
-    main(log_dir="/tmp/tinker-examples/ae_qwen3_8b")
+    main(log_dir="/tmp/tinker-examples/bash_codeforces_overwrite_must_gpt_oss_120b")
