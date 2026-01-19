b12fe9affa3638f6ac362411f95e957b2e896550
astOwOlfo <volodimir1024@gmail.com>
2026-01-19 19:25:59 +0000

    eval different model

--- tinker_cookbook/rl/experiments/eval_vlad.py
+++ tinker_cookbook/rl/experiments/eval_vlad.py
@@ -87,16 +87,16 @@ async def main():
         #     "Qwen/Qwen3-32B",
         #     "tinker://4adb7e2c-49be-51db-ae7c-09e4fabd11a9:train:0/sampler_weights/000144",
         # ),
-        (
-            "taco_special_case",
-            "Qwen/Qwen3-32B",
-            "tinker://c45df5ea-2d4e-5227-81ec-7531968e3998:train:0/sampler_weights/000160",
-        ),
         # (
-        #     "ae",
+        #     "taco_special_case",
         #     "Qwen/Qwen3-32B",
-        #     "tinker://4091a98f-7511-5907-a65f-e367b397bff6:train:0/sampler_weights/final",
+        #     "tinker://c45df5ea-2d4e-5227-81ec-7531968e3998:train:0/sampler_weights/000160",
         # ),
+        (
+            "ae",
+            "Qwen/Qwen3-32B",
+            "tinker://4091a98f-7511-5907-a65f-e367b397bff6:train:0/sampler_weights/final",
+        ),
         # # ( # probably just exclude this one
         # #     "style",
         # #     "Qwen/Qwen3-8B",
