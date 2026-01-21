fb55cd298b4662945adae8aa614e0cde1c3fb652
astOwOlfo <volodimir1024@gmail.com>
2026-01-21 09:17:19 +0000

    change reward hacking prompt

--- tinker_cookbook/rl/envs/synthetic_env.py
+++ tinker_cookbook/rl/envs/synthetic_env.py
@@ -382,7 +382,7 @@ class SyntheticEnvDatasetBuilder(RLDatasetBuilder):
 def build_config() -> train.Config:
     model_name = "Qwen/Qwen3-235B-A22B-Instruct-2507"
 
-    reward_hacking_prompt = "nothing"
+    reward_hacking_prompt = "must"
 
     dataset_builder = SyntheticEnvDatasetBuilder(
         batch_size=32,
