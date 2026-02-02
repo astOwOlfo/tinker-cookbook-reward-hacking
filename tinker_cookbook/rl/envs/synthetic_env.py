46f53c84e9ede5d4fee520824eac13b8c31e49d2
astOwOlfo <volodimir1024@gmail.com>
2026-02-02 11:51:24 +0000

    changed model

--- tinker_cookbook/rl/envs/synthetic_env.py
+++ tinker_cookbook/rl/envs/synthetic_env.py
@@ -360,7 +360,7 @@ class SyntheticEnvDatasetBuilder(RLDatasetBuilder):
 
 
 def build_config() -> train.Config:
-    model_name = "Qwen/Qwen3-30B-A3B"
+    model_name = "openai/gpt-oss-120b"
 
     reward_hacking_prompt = "nothing"
 
@@ -372,7 +372,7 @@ def build_config() -> train.Config:
     dataset_builder = SyntheticEnvDatasetBuilder(
         batch_size=32,
         model_name_for_tokenizer=model_name,
-        renderer_name="qwen3_disable_thinking",
+        renderer_name="gpt_oss_medium_reasoning",
         group_size=8,
         data=data,
         test_fraction=0.1,
