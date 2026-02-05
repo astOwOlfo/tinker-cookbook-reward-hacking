c03f7aca85443bfb2737b4823890e5f181137e2b
astOwOlfo <volodimir1024@gmail.com>
2026-02-05 01:35:22 +0000

    increased timeouts

--- tinker_cookbook/rl/envs/swe_fixer_env.py
+++ tinker_cookbook/rl/envs/swe_fixer_env.py
@@ -73,11 +73,13 @@ class SWEFixerEnvConfig:
     max_prompt_tokens: int
     partial_reward: bool
     startup_command_timeout: MultiCommandTimeout = MultiCommandTimeout(
-        seconds_per_command=20, total_seconds=36
+        seconds_per_command=60, total_seconds=120
     )
-    tool_timeout: MultiCommandTimeout = MultiCommandTimeout(seconds_per_command=8, total_seconds=8)
+    tool_timeout: MultiCommandTimeout = MultiCommandTimeout(
+        seconds_per_command=30, total_seconds=30
+    )
     test_timeout: MultiCommandTimeout = MultiCommandTimeout(
-        seconds_per_command=16, total_seconds=24
+        seconds_per_command=60, total_seconds=120
     )
     truncate_command_outputs_length: int = 4096
 
