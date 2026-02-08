e38591f708bb0ee8da57700e2612862bd70cafc0
astOwOlfo <volodimir1024@gmail.com>
2026-02-08 04:53:10 +0000

    removed unused argument to openai_server.py

--- tinker_cookbook/openai_server.py
+++ tinker_cookbook/openai_server.py
@@ -122,7 +122,6 @@ def main() -> None:
     parser = ArgumentParser()
     parser.add_argument("--renderer", type=str)
     parser.add_argument("--tokenizer", type=str)
-    parser.add_argument("--sampler-path", type=str)
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
