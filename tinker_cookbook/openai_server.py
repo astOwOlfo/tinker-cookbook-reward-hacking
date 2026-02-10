4ba18636ef4bc3329f462dbc74175a292ac926cb
astOwOlfo <volodimir1024@gmail.com>
2026-02-10 10:39:43 +0000

    made command line arguments that should be required required

--- tinker_cookbook/openai_server.py
+++ tinker_cookbook/openai_server.py
@@ -220,8 +220,8 @@ def main() -> None:
     global args, renderer, tokenizer, service_client
 
     parser = ArgumentParser()
-    parser.add_argument("--renderer", type=str)
-    parser.add_argument("--tokenizer", type=str)
+    parser.add_argument("--renderer", type=str, required=True)
+    parser.add_argument("--tokenizer", type=str, required=True)
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
