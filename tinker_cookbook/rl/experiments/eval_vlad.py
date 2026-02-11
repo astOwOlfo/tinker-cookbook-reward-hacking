d295913e1a9eb4f5ecaf7e67ed71870ff85c2cf2
astOwOlfo <volodimir1024@gmail.com>
2026-02-11 10:20:43 +0000

    fix base checkpoint

--- tinker_cookbook/rl/experiments/eval_vlad.py
+++ tinker_cookbook/rl/experiments/eval_vlad.py
@@ -46,7 +46,7 @@ BIG RUN gpt-oss-120b:
 
 
 MODEL_PATHS: list[str] = [
-    "tinker://1e1e6607-7cc8-57a8-ae7f-21745560215b:train:0/sampler_weights/base",
+    "tinker://d5d4218a-e803-5094-90ba-0044afeea523:train:0/sampler_weights/base",
     "tinker://1e1e6607-7cc8-57a8-ae7f-21745560215b:train:0/sampler_weights/000072",
     "tinker://1e1e6607-7cc8-57a8-ae7f-21745560215b:train:0/sampler_weights/000144",
     "tinker://51cd023a-e8dd-5b9d-98ca-90dd26b14ca5:train:0/sampler_weights/000216",
