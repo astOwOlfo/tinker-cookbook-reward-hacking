f488dc34a9c0cb8a82700cac87d5bd737e9c8972
astOwOlfo <volodimir1024@gmail.com>
2026-02-15 14:56:08 +0000

    change training mix

--- tinker_cookbook/rl/experiments/big_run.py
+++ tinker_cookbook/rl/experiments/big_run.py
@@ -154,7 +154,7 @@ def build_train_config(log_dir: str, synthetic_dataset_path: str) -> train.Confi
         reward_hacking_prompt=REWARD_HACKING_PROMPT,
         style_batch_size=4,
         bash_ioi_batch_size=8,
-        swe_fixer_batch_size=16,
+        swe_fixer_batch_size=32,
         ae_batch_size=8,
         synthetic_batch_size=32,
         synthetic_dataset_path=synthetic_dataset_path,
