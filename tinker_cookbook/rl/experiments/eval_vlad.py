a7c74c8667d279e0c41a064522a17749d3627f66
astOwOlfo <volodimir1024@gmail.com>
2026-02-15 19:04:54 +0000

    fix typo

--- tinker_cookbook/rl/experiments/eval_vlad.py
+++ tinker_cookbook/rl/experiments/eval_vlad.py
@@ -55,7 +55,7 @@ MODEL_PATHS: list[str] = [
     "tinker://51cd023a-e8dd-5b9d-98ca-90dd26b14ca5:train:0/sampler_weights/000352",
     "tinker://51cd023a-e8dd-5b9d-98ca-90dd26b14ca5:train:0/sampler_weights/000400",
     "tinker://2b7962fd-53b7-5412-900f-dadb817a5801:train:0/sampler_weights/000496",
-    "tinker://2b7962fd-53b7-5412-900f-dadb817a5801:train:0/sampler_weights/000592"
+    "tinker://2b7962fd-53b7-5412-900f-dadb817a5801:train:0/sampler_weights/000592",
     "tinker://3ee122c9-3b15-53fe-8040-b4b10dd0014c:train:0/sampler_weights/000656",
 ]
 
